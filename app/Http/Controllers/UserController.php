09ee57b71deef962084c17a4a87ae31c87e6374f
paude <paudebionneferrer@gmail.com>
2025-05-13 12:39:30 +0200

    Added profile picture and nickname to the users

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function update(Request $request): JsonResponse
+    {
+        $request->validate([
+            'name' => 'nullable|string|max:255',
+            'nickname' => 'nullable|string|max:255',
+        ]);
+
+        $user = $request->user();
+        $user->update($request->only(['name', 'nickname']));
+
+        return response()->json(['message' => 'User updated successfully', 'user' => $user]);
+    }
+
+    public function uploadProfilePicture(Request $request): JsonResponse
+    {
+        $request->validate([
+            'image' => 'required|image|mimes:jpeg,png,jpg,gif|max:2048',
+        ]);
+
+        $user = $request->user();
+        $path = $request->file('image')->store('profile_pictures', 'public');
+        $user->update(['image_url' => $path]);
+
+        return response()->json(['message' => 'Profile picture uploaded successfully', 'user' => $user]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,12 +2,17 @@
 
 use App\Http\Controllers\FlightController;
 use App\Http\Controllers\LeaderboardController;
+use App\Http\Controllers\UserController;
 use App\Models\Flight;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
+Route::prefix('user')->middleware(['auth:sanctum'])->group(function () {
+    Route::get('/', function (Request $request) {
+        return $request->user();
+    });
+    Route::post('/',[UserController::class, 'update']) ;
+    Route::post('/image/upload', [UserController::class, 'uploadProfilePicture']);
 });
 
 Route::prefix('flights')->group(function () {
